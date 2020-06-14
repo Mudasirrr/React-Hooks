8729a976e0424d46a6cd28d9625614b6700206c5
Mudasirrr <hmudasir714@gmail.com>
2020-06-14 03:39:44 -0400

    Updates

---
+++ precache-manifest.2a9ae33ca3db053e10d7041983a09c12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28354a1de4e24fa28488f0ad0157af11",
+    "url": "/React-Hooks/index.html"
+  },
+  {
+    "revision": "97ab706fe015c2d11af3",
+    "url": "/React-Hooks/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bbe112a523bb57e60ffe",
+    "url": "/React-Hooks/static/js/2.f8ac9c34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Hooks/static/js/2.f8ac9c34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97ab706fe015c2d11af3",
+    "url": "/React-Hooks/static/js/main.50574efa.chunk.js"
+  },
+  {
+    "revision": "7d05e063022071290f7f",
+    "url": "/React-Hooks/static/js/runtime-main.23fe235e.js"
+  }
+]);
